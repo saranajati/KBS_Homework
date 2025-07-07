5d039de6ffff3da7328c990e252a17164028222f
sara najati <saranajatilaptop@gmail.com>
2025-07-08 00:58:44 +0300

    bfs

--- src/main.py
+++ src/main.py
@@ -1,22 +1,25 @@
 from experta import Fact
 from engine import BridgePuzzleSolver
+from facts import *
+from algorithms import *
 
 
 def main():
     print("BRIDGE PUZZLE (KBS Homework)")
     print("-" * 100)
 
-    travel_time = [
-        ("You", 1),
-        ("Lab Assistant", 2),
-        ("Worker", 5),
-        ("Scientist", 10)
-    ]
+    travel_time = [("You", 1), ("Lab Assistant", 2), ("Worker", 5), ("Scientist", 10)]
     max_time = 17
 
-    engine = BridgePuzzleSolver(travel_time, max_time)
-    engine.reset()
-    engine.run()
+    # engine = BridgePuzzleSolver(travel_time, max_time)
+    # engine.reset()
+    # engine.run()
+
+    strategy_input = input("Choose search strategy (bfs / dfs): ").strip().lower()
+    controller = BridgePuzzleAlgorithms(people=travel_time, max_time=max_time)
+    controller.reset()
+    controller.declare(SearchAlgorithm(name=strategy_input))
+    controller.run()
 
 
 if __name__ == "__main__":
